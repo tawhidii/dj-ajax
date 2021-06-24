38dcb601150f4ab775a7adf740d32bdd167b0648
khademul bari <khademul.py@gmail.com>
2021-06-25 01:47:15 +0600

    :sparkles: test basic DOM manupulation

---
+++ posts/static/post/post.js
@@ -0,0 +1,2 @@
+let hellWorld = document.getElementById('hello-world')
+hellWorld.innerHTML = '<h1>Hello world </h1>'
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Posts
 
-# Create your views here.
+
+def post_list_and_create(request):
+    qs = Posts.objects.all()
+    context = {
+        'qs': qs
+    }
+    return render(request, 'posts/post.html', context)
