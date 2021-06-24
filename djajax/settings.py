27dd3a55ad9276c21c4f73191a75c1391be6ad58
khademul bari <khademul.py@gmail.com>
2021-06-24 21:49:46 +0600

    :sparkles: added post and profile app

--- djajax/settings.py
+++ djajax/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # custom apps
+    'posts.apps.PostsConfig',
+    'profiles.apps.ProfilesConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
