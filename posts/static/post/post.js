f8e5d1d6699b7be66e1f54845d416e4824dea9b1
barii <khademul.py@gmail.com>
2021-07-15 23:47:09 +0600

    :triangular_flag_on_post: updated csrf

--- posts/static/post/post.js
+++ posts/static/post/post.js
@@ -4,6 +4,47 @@ let spinnerBox = document.getElementById('spinner-box')
 let loadBtn = document.getElementById('load-btn')
 let loadMoreEnd = document.getElementById('end-load')
 
+const getCookie =  name => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+const likeUnlikePost = () =>{
+    const likeUnLikeForm = document.getElementsByClassName('like-unlike-form')
+    const likeUnlike = [...likeUnLikeForm]
+    likeUnlike.forEach(form=>form.addEventListener('submit',event=>{
+        event.preventDefault()
+        const clickedId = event.target.getAttribute('data-form-id')
+        const clickedBtn = document.getElementById(`like-unlike-${clickedId}`)
+        $.ajax({
+            type: 'POST',
+            url: "",
+            data:{
+                'csrfmiddlewaretoken': csrftoken,
+                'pk':clickedId,
+            },
+            success:function (response){
+                console.log(response)
+            },
+            error:function (error){
+                console.log(error)
+            }
+        })
+    }))
+
+}
 
 let visible = 3
 
@@ -16,24 +57,29 @@ const getData = () => {
         spinnerBox.classList.add('not-visible')
         setTimeout(()=>{
             data.forEach(ele=>{
-            postBox.innerHTML += `<div class="card mb-2">
-                                      <div class="card-body">
-                                        <h5 class="card-title">${ele?.title}</h5>
-                                        <p class="card-text">${ele?.body}</p>          
-                                      </div>
-                                       <div class="card-footer">
-                                            
-                                            <div class="row">
-                                            <div class="col-1">
-                                                <a href="#" class="btn btn-primary">Details</a>
-                                            </div>
-                                            <div class="col-1">
-                                                <a href="#" class="btn btn-primary">${ ele.liked ? `Unlike(${ele.count})`:`Like(${ele.count})`}</a>
-                                            </div>
-                                            </div>
-                                        </div>
-                                   </div>`
+            postBox.innerHTML +=
+        `<div class="card mb-2">
+                              <div class="card-body">
+                                <h5 class="card-title">${ele?.title}</h5>
+                                <p class="card-text">${ele?.body}</p>          
+                              </div>
+                               <div class="card-footer">
+                                    
+                                    <div class="row">
+                                    <div class="col-1">
+                                        <a href="#" class="btn btn-primary">Details</a>
+                                    </div>
+                                    <div class="col-1">
+                                        <form class="like-unlike-form" data-form-id="${ele.id}">
+                       
+                                            <button href="#" class="btn btn-primary">${ ele.liked ? `Unlike(${ele.count})`:`Like(${ele.count})`}</button>
+                                        </form>
+                                    </div>
+                                    </div>
+                                </div>
+                           </div>`
                         })
+
         },500)
         if(response.size===0){
             loadMoreEnd.textContent = 'No post yet posted'
