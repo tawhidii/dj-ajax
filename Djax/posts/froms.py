fd185b052e1a90669917c42ff69a2d9abcb368e3
Kohnodker Khademul Bari <bari35-1159@diu.edu.bd>
2021-09-07 16:04:14 +0600

    :triangular_flag_on_post: Added form in post modal

---
+++ Djax/posts/froms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Posts
+
+
+class PostForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    body = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3}))
+
+    class Meta:
+        model = Posts
+        fields = ('title', 'body',)
