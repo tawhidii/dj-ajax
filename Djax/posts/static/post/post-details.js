fe32ed3f9fbbbbbfee6bddba93d23ccbdc2b49dd
tawhidii <barii.py@gmail.com>
2021-09-16 17:45:23 +0600

    Some changes in post details

--- Djax/posts/static/post/post-details.js
+++ Djax/posts/static/post/post-details.js
@@ -1,1 +1,32 @@
-console.log('hello from post details')
+const backBtn = document.getElementById('back-btn')
+const url = `${window.location.href}data/`
+const spinnerBox = document.getElementById('spinner-box')
+const editBtn = document.getElementById('edit-btn')
+const deleteBtn = document.getElementById('delete-btn')
+
+backBtn.addEventListener('click',()=>{
+    history.back()
+})
+
+// get post details via ajax request
+const postDetailsData = () => {
+    $.ajax({
+        type:'GET',
+        url:url,
+        success:(response)=>{
+            console.log(response)
+            spinnerBox.classList.add('not-visible')
+            const data = response.data
+            if(data.logged_in !== data.author){
+                 console.log('Different')
+            }else {
+                editBtn.classList.remove('not-visible')
+                deleteBtn.classList.remove('not-visible')
+            }
+        },
+        error:(error)=>{
+            console.log(error)
+        }
+    })
+}
+postDetailsData()
--- Djax/posts/urls.py
+++ Djax/posts/urls.py
@@ -3,14 +3,16 @@ from .views import (
     post_list_and_create,
     load_posts,
     like_unlike_post,
-    post_details
+    post_details,
+    post_details_data_view
 )
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='post-main'),
-    path('post-data/<int:num_posts>/',load_posts,name='post-data'),
-    path('like-unlike/',like_unlike_post,name='like-unlike'),
-    path('<pk>/',post_details,name='post-detail')
+    path('post-data/<int:num_posts>/', load_posts, name='post-data'),
+    path('like-unlike/', like_unlike_post, name='like-unlike'),
+    path('<pk>/', post_details, name='post-detail'),
+    path('<pk>/data/', post_details_data_view, name='post-detail-data-view')
 ]
